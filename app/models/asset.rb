9641aec99d2d994677683c510044191504032630
Stephan Seidt <stephan@uberhost.de>
2009-08-13 04:27:06 +0200

    Modified paperclip paths

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,6 +2,6 @@ class Asset < ActiveRecord::Base
   belongs_to :user
   
   has_attached_file :file,
-    :url  => "/assets/:class/:id/:style.:extension",
-    :path => ":rails_root/public/assets/:class/:id/:style.:extension"
+    :url  => "/system/assets/:class/:id/:style.:extension",
+    :path => ":rails_root/public/system/assets/:class/:id/:style.:extension"
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,8 @@ class Image < Asset
   belongs_to :collection
   
   has_attached_file :file,
-    :url  => "/assets/:class/:id/:style.:extension",
-    :path => ":rails_root/public/assets/:class/:id/:style.:extension",
+    :url  => "/system/assets:class/:id/:style.:extension",
+    :path => ":rails_root/public/system/assets/:class/:id/:style.:extension",
     :styles => {
       :thumbnail          => "100x140",
       :thumbnail_square   => "100x100#",
