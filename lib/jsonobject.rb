2699eec4a8e4faf0ebf51d204e11e2bf123e0bb8
Stephan Seidt <stephan@uberhost.de>
2009-08-31 16:51:23 +0200

    Added rough schema-functionality to JsonObject

--- lib/jsonobject.rb
+++ lib/jsonobject.rb
@@ -8,6 +8,13 @@
 # JsonObject
 module JsonObject
   class JsonItem < HashWithIndifferentAccess # Hash
+    attr_accessor :schema
+    
+    def initialize(hash, schema)
+      self.schema = schema
+      super(hash)
+    end
+    
     def try(name)
       if self[name]
         self[name]
@@ -25,6 +32,12 @@ module JsonObject
     end
     
     def method_missing(method_name, *args) 
+      if self.schema
+        self.schema.each do |i|
+          return self[i['uid']] if method_name.to_s == i['name']
+        end
+      end
+      
       self[method_name.to_sym]
     end
   end
@@ -40,35 +53,39 @@ module JsonObject
   end
   
   module ClassMethods
-    def has_json_object(name_as_sym, options = {})
+    def has_json_object(name, options = {})
       extend JsonObject::SingletonMethods
       include JsonObject::InstanceMethods
       
       write_inheritable_attribute(:json_object_definitions, {}) if json_object_definitions.nil?
-      json_object_definitions[name_as_sym] = options
+      json_object_definitions[name] = options
       
-      JsonObject::InstanceMethods.send "define_method", "#{name_as_sym.to_s}" do |*args|
-        if self.send("#{name_as_sym.to_s}_json")
-          object = ActiveSupport::JSON.decode(self.send("#{name_as_sym.to_s}_json"))
+      JsonObject::InstanceMethods.send "define_method", "#{name}" do |*args|
+        if self.send("#{name}_json")
+          object = ActiveSupport::JSON.decode(self.send("#{name}_json"))
           
           # after_read callback
-          after_read = self.class.json_object_definitions[name_as_sym][:after_read]
+          after_read = self.class.json_object_definitions[name][:after_read]
           object = after_read.call(self, object) unless after_read.nil?
+          
+          # schema
+          schema = self.class.json_object_definitions[name][:schema]
+          if schema
+            object = JsonItem.new(object, schema.call(self))
+          end
           
           object
         else
-          self.class.json_object_definitions[name_as_sym][:default]
+          self.class.json_object_definitions[name][:default]
         end
       end
       
-      JsonObject::InstanceMethods.send "define_method", "#{name_as_sym.to_s}=" do |*args|
-        object = args[0]
-        
+      JsonObject::InstanceMethods.send "define_method", "#{name}=" do |object|
         # before_serialize callback
-        before_serialize = self.class.json_object_definitions[name_as_sym][:before_serialize]
+        before_serialize = self.class.json_object_definitions[name][:before_serialize]
         object = before_serialize.call(object) unless before_serialize.nil?
         
-        self.send "#{name_as_sym.to_s}_json=", object.to_json
+        self.send "#{name}_json=", object.to_json
       end
     end
     
@@ -77,4 +94,3 @@ module JsonObject
     end
   end
 end
-
