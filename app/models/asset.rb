3ea569b3e497e45aca8ab46f9123a7fa023ae73b
Mathias Schaefer (molily) <zapperlott@gmail.com>
2009-09-17 12:10:27 +0200

    code formatting

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -6,8 +6,8 @@ class Asset < ActiveRecord::Base
   belongs_to :collection
   
   has_attached_file :file,
-    :url  => "/system/assets/:class/:id/:style.:extension",
-    :path => ":rails_root/public/system/assets/:class/:id/:style.:extension"
+    :path => ":rails_root/public/system/assets/:class/:id/:style.:extension",
+    :url  =>                   "/system/assets/:class/:id/:style.:extension"
   
   def serialize
     file.url
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,11 +2,12 @@ class Image < Asset
   belongs_to :collection
   
   has_attached_file :file,
-    :url  => "/system/assets/:class/:id/:style.:extension",
     :path => ":rails_root/public/system/assets/:class/:id/:style.:extension",
+    :url  =>                   "/system/assets/:class/:id/:style.:extension",
     :styles => {
       :thumbnail          => "100x140",
       :thumbnail_square   => "100x100#",
       :full               => "300x250"
     }
+    
 end
