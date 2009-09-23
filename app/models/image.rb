190fca9174b74a492bc201a39d7423ee284beb2b
Mathias Schaefer (molily) <zapperlott@gmail.com>
2009-09-23 22:01:01 +0200

    new image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,7 +8,8 @@ class Image < Asset
       :vrame_backend      => "100x50",
       :thumbnail          => "100x140",
       :thumbnail_square   => "100x100#",
-      :full               => "300x250"
+      :full               => "300x250",
+      :bg_thumb           => "392x272"
     }
     
 end
