4f07fea5ece3551214e8aa3370cb8010af04a78c
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2009-09-23 03:26:13 +0200

    Made nasty regex to make droppable more flexible

--- public/vrame/javascripts/jquery/jquery.html5sortable.js
+++ public/vrame/javascripts/jquery/jquery.html5sortable.js
@@ -6,10 +6,12 @@
 	
         opts = $.extend({
 				itemSelector      : 'li.item',
+				droppableSelector : 'li.droppable',
 				onDrop			  : noop
 			},
 			opts || {}
 		);
+		opts.droppableClass = opts.droppableSelector.match(/\.([^ ]+)/)[1];
 
 		var animateHelpers = function() {
 			$('li.helper', list).each(function(i, el) {
@@ -33,6 +35,10 @@
 			
 			$(opts.itemSelector, list).each(function(i, el) {
 				$(el).before('<li class="helper" data-relative="before" data-id="' + el.id + '">&nbsp;</li>');
+				$(el).after('<li class="helper" data-relative="after" data-id="' + el.id + '">&nbsp;</li>');
+			});
+
+			$(opts.droppableSelector, list).each(function(i, el) {
 				$(el).after('<li class="helper" data-relative="after" data-id="' + el.id + '">&nbsp;</li>');
 			});
 			
@@ -96,9 +102,7 @@
 			});
 		}
 		
-		var makeElementSortable = function(el) {
-			var currentEl = null;
-
+		var makeElementDraggable = function(el) {
 			// make the item draggable
 			el.attr('draggable', 'true');
 			
@@ -110,7 +114,9 @@
 				e.originalEvent.dataTransfer.effectAllowed = 'move';
 				e.originalEvent.dataTransfer.setData('Text', this.id); // required otherwise doesn't work				
 			});
-			
+		}
+		
+		var makeElementDroppable = function(el) {
 			var dragOverHelperFunction = function(e) {				
 				// disable all helpers
 				$('li.helper', list).each(function(i, el) {
@@ -123,19 +129,25 @@
 					if(target.get(0)) {
 						var targetY = target.get(0).offsetTop;
 						var targetH = target.height() * 2; // @TODO, why 2* height?
-					
-						if(e.pageY-targetY < targetH/2) {
-							target.prev().css({'display' : 'block'});
-							target.prev().get(0).toHeight = targetH;
-							target.next().get(0).toHeight = 0;
-							target.next().get(0).myHeight = 0;
-							target.get(0).sortableInsert = 'before';
-						} else {
-							target.prev().get(0).toHeight = 0;
-							target.prev().get(0).myHeight = 0;
+						
+						if(target.hasClass(opts.droppableClass)) {
 							target.next().css({'display' : 'block'});
 							target.next().get(0).toHeight = targetH;
 							target.get(0).sortableInsert = 'after';
+						} else {
+							if(e.pageY-targetY < targetH/2) {
+								target.prev().css({'display' : 'block'});
+								target.prev().get(0).toHeight = targetH;
+								target.next().get(0).toHeight = 0;
+								target.next().get(0).myHeight = 0;
+								target.get(0).sortableInsert = 'before';
+							} else {
+								target.prev().get(0).toHeight = 0;
+								target.prev().get(0).myHeight = 0;
+								target.next().css({'display' : 'block'});
+								target.next().get(0).toHeight = targetH;
+								target.get(0).sortableInsert = 'after';
+							}							
 						}
 					}
 				}
@@ -157,13 +169,21 @@
 			el.bind('dragend', function (e) {
 				removeHelpers();
 			});
+		}
+		
+		var makeElementSortable = function(el) {
+			makeElementDraggable(el);
+			makeElementDroppable(el);
 		};
 
-		var items = $(opts.itemSelector, list);
-
 		// make all elements sortable
-		items.each(function(i, el) {
+		$(opts.itemSelector, list).each(function(i, el) {
 			makeElementSortable($(el));
+		});
+		
+		// make some elements droppable
+		$(opts.droppableSelector, list).each(function(i, el) {
+			makeElementDroppable($(el));
 		});
 	};
 })(jQuery);
