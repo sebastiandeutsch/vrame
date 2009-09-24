e895711aa42e9f2ab9fe35c00c7c554490f93145
Mathias Schaefer (molily) <zapperlott@gmail.com>
2009-09-24 18:30:42 +0200

    paperclip is_image? instance method

--- lib/paperclip_images.rb
+++ lib/paperclip_images.rb
@@ -1,8 +1,14 @@
 module Paperclip
   class Attachment
     ImageFileExtensions = %w(jpg png gif)
+    
     def self.is_image? (filename)
       ImageFileExtensions.include?(File.extname(filename)[1..-1])
     end
+    
+    def is_image? ()
+      ImageFileExtensions.include?(File.extname(original_filename)[1..-1])
+    end
+    
   end
 end
