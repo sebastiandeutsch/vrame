036c67b9a89ecf90fa3461fd85da972cab00e8cc
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2009-10-09 01:09:17 +0200

    Added gem dependencies to init.rb

--- init.rb
+++ init.rb
@@ -4,6 +4,35 @@
 require 'jsonobject'
 ActiveRecord::Base.send :include, JsonObject
 
+config.gem 'coupa-acts_as_list',
+  :lib => 'coupa-acts_as_list',
+  :source => 'http://gems.github.com'
+
+config.gem 'coupa-acts_as_tree',
+  :lib => 'coupa-acts_as_tree',
+  :source => 'http://gems.github.com'
+
+config.gem 'binarylogic-authlogic',
+  :lib     => 'authlogic',
+  :source  => 'http://gems.github.com'
+
+config.gem 'mislav-will_paginate',
+  :lib => 'will_paginate',
+  :source => 'http://gems.github.com'
+
+config.gem 'mini_magick',
+  :lib => 'mini_magick'
+
+config.gem 'thoughtbot-paperclip',
+  :lib => 'paperclip',
+  :source => 'http://gems.github.com'
+
+config.gem 'norman-friendly_id',
+  :lib => 'friendly_id',
+  :source => 'http://gems.github.com'
+    
+config.gem 'daemons'
+
 # Include VrameHelper
 config.to_prepare do
   ApplicationController.helper(VrameHelper)
