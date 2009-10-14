a5a34be6164f2407571fa1540a2d663a5a71d886
Jan Varwig <jan@varwig.org>
2009-10-14 17:00:34 +0200

    Modified init.rb to clear load_once_paths in development environment

--- init.rb
+++ init.rb
@@ -1,5 +1,9 @@
 # Include hook code here
 
+load_paths.each do |path|
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end if config.environment == 'development'
+
 # Tell ActiveRecord::Base about JsonObject
 require 'jsonobject'
 
