3e96972a727f1f6d5c9c5c077d9f84b52f64fcf4
Jan Varwig <jan@varwig.org>
2009-10-30 17:25:50 +0100

    Began with making assets work again

--- lib/jsonobject/types/asset.rb
+++ lib/jsonobject/types/asset.rb
@@ -2,6 +2,9 @@ module JsonObject
   module Types
     class Asset < JsonObject::Type
       attr_accessor :styles
+      def object_from_value(val)
+        ::Asset.find_by_id(val.to_i)
+      end
     end
   end
 end
