41425ad4475e5c67b2b1ff134ac6a39dbd85d947
Jan Varwig <jan@varwig.org>
2009-11-03 20:32:29 +0100

    JsonObject::Types::Collection#object_from_value implemented

--- lib/jsonobject/types/collection.rb
+++ lib/jsonobject/types/collection.rb
@@ -1,6 +1,9 @@
 module JsonObject
   module Types
     class Collection < JsonObject::Type
+      def object_from_value(val)
+        ::Collection.find_by_id(val.to_i)
+      end
     end
   end
 end
