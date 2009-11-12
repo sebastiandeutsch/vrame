3b9d5252878caf3146cb4d11f7b8529f09594875
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2009-11-12 18:35:30 +0100

    VrameController overwrites standard language mechanism

--- app/controllers/vrame/vrame_controller.rb
+++ app/controllers/vrame/vrame_controller.rb
@@ -2,25 +2,25 @@ class Vrame::VrameController < ApplicationController
   layout 'vrame'
   
   before_filter :require_user
-  before_filter :choose_language, :except => :select_language
 
-  def select_language
-    session[:admin_current_language_id] = params[:id]
-    redirect_to '/admin'
+  def switch_language
+    @current_language = Language.find(params[:id])
+    session["vrame_backend_language_id"] = @current_language.id
+    
+    redirect_to vrame_root_path
   end
   
 private
-  def choose_language
-    if session[:admin_current_language_id]
-      @admin_current_language = Language.find_by_iso3_code(session[:admin_current_language_id])
+  def select_language
+    if session["vrame_backend_language_id"]
+      @current_language = Language.find(session["vrame_backend_language_id"])
     else
-      @admin_current_language = Language.all.first
+      @current_language = Language.all.first
     end
     
-    if @admin_current_language.nil?
-      render :text => 'No languages found please run "rake vrame:bootstrap"'
-    else
-      session[:admin_current_language_id] = @admin_current_language.iso3_code
-    end
+    raise NoLanguageInDatabaseError, "You have to add a language to the database" if @current_language.nil?
+    session["vrame_backend_language_id"] = @current_language.id
+
+    true
   end
 end
