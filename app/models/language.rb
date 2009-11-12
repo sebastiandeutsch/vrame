3ebc7ae9176a199708d3da415ac5042952c94ed0
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2009-11-12 18:34:31 +0100

    Language model revisited

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,9 +1,9 @@
 class Language < ActiveRecord::Base
-  has_many :templates, :order => :position
-  has_many :documents, :order => :position
+  has_many :categories, :order => :position
+  has_many :documents,  :order => :position
   
   validates_presence_of :name, :message => 'Das Feld darf nicht leer sein'
-  validates_presence_of :iso3_code, :message => 'Das Feld darf nicht leer sein'
+  validates_presence_of :iso2_code, :message => 'Das Feld darf nicht leer sein'
   
   named_scope :published, { :conditions => ["published = '1'"] }
 end
